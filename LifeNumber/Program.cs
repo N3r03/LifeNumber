378ae9b1ba36024e81586261e924da1edc8dfc45
nerminajakupovska@gmail.com <nerminajakupovska@gmail.com>
2019-11-15 18:50:48 +0100

    Add project files.

---
+++ LifeNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LifeNumber
+{
+    class Program
+    {
+
+        static int lifeNumber(int n)
+        {
+            int number = 0;
+
+            while (n > 0 || number > 9)
+            {
+                if (n == 0)
+                {
+                    n = number;
+                    number = 0;
+
+                }
+                number += n % 10;
+                n /= 10;
+
+            }
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            int t = 13021999;
+            Console.WriteLine(lifeNumber(t));
+        }
+    }
+}
